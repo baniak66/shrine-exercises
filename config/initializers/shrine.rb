ffe7de6733181270b5bdda31b1194af25b02bc4d
Paweł Licznar <pawellicznar@gmail.com>
2021-06-15 23:58:24 +0200

    s3 config

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,30 @@
 require "shrine"
 require "shrine/storage/file_system"
+require "shrine/storage/s3"
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
-}
+if Rails.env.test?
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new,
+  }
+elsif Rails.env.development?
+ Shrine.storages = {
+   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
+   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
+ }
+else
+  require "shrine/storage/s3"
+  s3_options = {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:            ENV['AWS_REGION'],
+    bucket:            ENV['AWS_BUCKET']
+  }
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+    store: Shrine::Storage::S3.new(prefix: "store", **s3_options)
+  }
+end
 
 Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
